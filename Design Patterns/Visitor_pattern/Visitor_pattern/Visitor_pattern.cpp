a77c5300a9ec6dd318865c90bf77d9cfb8298202
anoop3495 <anoop3495@gmail.com>
2021-07-18 12:08:53 +0200

    Desgin Patterns

---
+++ Design Patterns/Visitor_pattern/Visitor_pattern/Visitor_pattern.cpp
@@ -0,0 +1,23 @@
+#include "Visitor_pattern.h"
+
+void VisitorCaller::VisitCaller(Callercall* const)
+{
+    std::cout << "Visitor 1: Visited." << std::endl;
+}
+
+void Callercall::accept(Visitor& visitor)
+{
+    visitor.VisitCaller(this);
+}
+
+void callPattern1()
+{
+    Callercall call;
+    VisitorCaller visitor;
+    call.accept(visitor);
+}
+
+int main()
+{
+    return 0;
+}
